e00955fad98315e2e82ea443f56cf767616b5412
AustinVillanueva <avillann00@gmail.com>
2026-02-07 17:00:52 +0000

    feat(Register): Configured client and server
    features:
    - Implemented register endpoint on client side

--- backend/src/main/java/com/car_maintenance/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/car_maintenance/backend/security/SecurityConfig.java
@@ -4,23 +4,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.*;
+
+import java.util.List;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
-public class SecurityConfig{
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    http
-      .csrf(csrf -> csrf.disable())
-      .sessionManagement(session ->
-          session.sessionCreationPolicy(STATELESS)
-      )
-      .authorizeHttpRequests(auth -> auth
-          .requestMatchers("/auth/register", "/auth/login").permitAll()
-          .requestMatchers("/users/**").hasRole("ADMIN")
-          .anyRequest().authenticated()
-      );
+public class SecurityConfig {
 
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> {})
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/register", "/auth/login").permitAll()
+                .requestMatchers("/users/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
